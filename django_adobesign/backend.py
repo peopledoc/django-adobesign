d70731fac90ab01a35160e3c947e5cd2b6f29abd
dsteinberger <david.steinberger@people-doc.com>
2018-09-26 14:25:30 +0200

    Fix case sensitive from Adobe email field

--- django_adobesign/backend.py
+++ django_adobesign/backend.py
@@ -72,7 +72,7 @@ class AdobeSignBackend(django_anysign.SignatureBackend):
         for adobe_signer in self.get_all_signers(
                 signature.signature_backend_id).get('participantSets', []):
             # We only have 1 signer by turn
-            email = adobe_signer['memberInfos'][0]['email']
+            email = adobe_signer['memberInfos'][0]['email'].lower()
             signer = signature.signers.get(signing_order=adobe_signer['order'],
                                            email=email)
             signer.signature_backend_id = adobe_signer['id']
